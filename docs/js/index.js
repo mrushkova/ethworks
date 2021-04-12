a47ec1227377cd6c8d6161e2193b3d518e4a9232
Maria Rushkova <maryrushkova@gmail.com>
2021-04-13 01:45:17 +0200

    feet: add mq mobile

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,11 +1,9 @@
-"use strict";
-
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
   spaceBetween: 6,
   loop: true,
   pagination: {
     el: '.swiper-pagination',
-    clickable: true
-  }
+    clickable: true,
+  },
 });
